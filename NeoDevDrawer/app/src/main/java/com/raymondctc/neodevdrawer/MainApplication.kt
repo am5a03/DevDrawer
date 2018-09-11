4c0da63b629cd43f9d2e10f71c0737daa2a56b2a
Raymond Chan <am5a03@gmail.com>
2018-09-11 23:43:48 +0800

    Remove unnecessary code

--- NeoDevDrawer/app/src/main/java/com/raymondctc/neodevdrawer/MainApplication.kt
+++ NeoDevDrawer/app/src/main/java/com/raymondctc/neodevdrawer/MainApplication.kt
@@ -4,11 +4,6 @@ import android.app.Application
 import com.raymondctc.neodevdrawer.db.AppDatabase
 
 class MainApplication: Application() {
-
-    companion object {
-        var db: AppDatabase? = null
-    }
-
     override fun onCreate() {
         super.onCreate()
     }
